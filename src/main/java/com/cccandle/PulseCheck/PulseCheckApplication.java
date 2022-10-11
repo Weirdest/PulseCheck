c1a55ad9a0538c896f2174c651979b31ef74ed20
Justin Latta <webmaster@cccandle.com>
2022-10-11 09:31:34 -0600

    Init Commit

---
+++ src/main/java/com/cccandle/PulseCheck/PulseCheckApplication.java
@@ -0,0 +1,13 @@
+package com.cccandle.PulseCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cccandle/PulseCheck/PulseCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cccandle.PulseCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
